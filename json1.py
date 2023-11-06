7afdba2ba82dc792b9c3894046ef182afdfb74ab
oakerekan <gaiusanchez16@gmail.com>
2023-11-06 05:45:38 +0100

    initial commit

--- json1.py
+++ json1.py
@@ -1,21 +1,4 @@
-"""
-What we were given 
 
-Two json folders
-users information
-cards information 
-
-get the data into python,
-
-Create relationship with drawsql 
-python and SQL
-
-manipulate into python dictionary
-reconstructed into a csv file
-
-take the CSV from python into Dbeaver and intend to answer the question 
-we were given  
-"""
 
 import os
 import json
